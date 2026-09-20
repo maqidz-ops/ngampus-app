22ce78c17e11e2ea33901cb5534f84b51db45f0b
maqidz <muhtaqidzakwan@gmail.com>
2026-09-20 18:51:32 +0700

    chore: remove deprecated devIndicators.appIsrStatus config

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  devIndicators: {
-    appIsrStatus: false,
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
